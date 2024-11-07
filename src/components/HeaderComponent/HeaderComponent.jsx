1e2a338ccbc6c5bad5efb7c144d289bf5464d144
Artorias126 <127570423+Artorias126@users.noreply.github.com>
2024-11-07 20:23:18 +0700

    initial commit

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Col } from 'antd';
+import Search from 'antd/es/transfer/search';
+import {
+    UserOutlined,CaretDownOutlined,ShoppingCartOutlined,
+  } from '@ant-design/icons';
+import { WrapperHeader, WrapperTextHeader,WrapperHeaderAccount,WrapperTextHeaderSmall } from './style';
 
 function HeaderComponent() {
     return (
-        <div>HeaderComponent</div>
+        <div>
+            <WrapperHeader>
+                <Col span={6}>
+                    <WrapperTextHeader>DUYLAPTOP</WrapperTextHeader>
+                </Col>
+                <Col span={12}>
+                <Search 
+                placeholder="input search text" 
+                enterButton 
+                // onSearch={onSearch}
+                />
+                </Col>
+                <Col span={6} style ={{ display: 'flex', gap: '20px'}}>
+                <WrapperHeaderAccount>
+                <UserOutlined style ={{ fontSize: '30px'}}/>
+                <div>
+                    <WrapperTextHeaderSmall>Đăng Nhập/Đăng Kí</WrapperTextHeaderSmall>
+                    <div>
+                    <WrapperTextHeaderSmall>Tài Khoản</WrapperTextHeaderSmall>
+                    <CaretDownOutlined />
+                    </div>
+                </div>
+                </WrapperHeaderAccount>
+                <div>
+                    
+                <ShoppingCartOutlined style ={{ fontSize: '30px',color:'#fff'}} />
+                <WrapperTextHeaderSmall>Giỏ Hàng</WrapperTextHeaderSmall>
+                    
+                </div>
+                </Col>
+            </WrapperHeader>
+        </div>
     )
 }
 
